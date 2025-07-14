b070122cbdb99619422888c03d0397a355354cbc
Tejas <tejaskuranjekar10@gmail.com>
2025-07-14 10:08:53 +0530

    first commit

---
+++ Crud_API/Crud_API/src/test/java/Java_Ninjas/Crud_API/CrudApiApplicationTests.java
@@ -0,0 +1,13 @@
+package Java_Ninjas.Crud_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
